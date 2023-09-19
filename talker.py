2babac79b1448f5c61b49b7af3b9711d2e8b2862
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-09-19 07:49:18 -0400

    Update talker.py

--- talker.py
+++ talker.py
@@ -1049,7 +1049,7 @@ if countryQuestion == "y":
         print("Argentina is famous for its Dulce de Leche.")
     elif "canada" in countryConditionalY:
         sleep(1)
-        print("===============================================================================================================")
+        print("=======================================================================================")
         print("Canada has one of the most welcoming policies for immigrants and refugees.")
     elif "mexico" in countryConditionalY:
         sleep(1)
