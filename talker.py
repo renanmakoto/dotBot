73d31c447d80f56fa22b5217924e77362728ad10
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-01-12 07:32:04 -0500

    Update talker.py

--- talker.py
+++ talker.py
@@ -749,7 +749,7 @@ if "medical" and "doctor" in thirdQuestion:
             print("Well, ok, then.")
         elif fourthLoop == "no":
             sleep(1)
-            print("===============================================================================================================")
+            print("=======================================================================================")
             print("Well, ok, then.")
     elif "no" in thirdLoop:
         sleep(1)
